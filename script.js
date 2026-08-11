86db57feaf7b072f3f5bc2aea4b62af2c978cb95
aster4-foru <aster4.foru@gmail.com>
2026-08-11 20:25:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,19 @@
 // ASTER4｜JavaScript
 // =========================
 
+// =========================
+// ASTER4｜JavaScript
+// =========================
+
 console.log("ASTER4 script.js 載入成功！");
+
+
+// =========================
+// 取得網址上的商品 ID
+// =========================
+
+const params = new URLSearchParams(window.location.search);
+
+const productId = params.get("product");
+
+console.log("目前商品 ID：", productId);
